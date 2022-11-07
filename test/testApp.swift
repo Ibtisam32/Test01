23d44405b4c1137a058467d0b09dd23c6a0a75cb
Ibtisam <ihowaimel@gmail.com>
2022-11-07 15:17:26 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ibtisam on 13/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
